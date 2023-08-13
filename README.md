91bf871d20a42658f437cd5fa8495ffc336903ee
Thomas Manthey <mail@thomas-manthey.de>
2023-08-13 11:40:12 +0200

    Replaced the not so nice flame animation to a simulation of the berlin clock.

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ Adaptation of several examples for the Pimoroni Unicorn Pico in one file.
 
 - Button X starts a pomodoro timer with the usual 25/5 timing.
 - Button A starts a simulation of a super computer blinking display like seen in many 80's movies.
-- Button B starts a fire simulation - get some warmth on your desk!
+- Button B starts a berlin clock simulation
 - Button Y stops the running routine and clears the screen.
+
+# Berlin clock
+
+Once the clock is running you can use BUTTON_X to change into settings mode.
+BUTTON_A adjusts the hours, BUTTON_B adjusts the minutes. Pressing BUTTON_X again saves the time to the RTC.
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import math
 import utime
 import urandom as random
+import machine
 from picounicorn import PicoUnicorn
 
 unicorn = PicoUnicorn()
-
+clock = machine.RTC()
+BUTTON_IDLE_TIME = 250
 
 def clrscr():
     for x in range(16):
@@ -136,12 +138,109 @@ def flames():
         
         utime.sleep(0.05)
 
+@micropython.native  # noqa: F821
+def berlin_clock():
+    
+    MODE_SETTIME = 1
+    MODE_RUNCLOCK = 2
+    
+    handling_a_ts = utime.ticks_ms()
+    handling_b_ts = utime.ticks_ms()
+    handling_x_ts = utime.ticks_ms()
+    
+    mode = MODE_RUNCLOCK
+    
+    # inner function to draw a lamp
+    def draw_lamp(xs, ys, w, h, r, g, b):
+        for x in range(xs, xs+w):
+            for y in range(ys, ys+h):
+                unicorn.set_pixel(x, y, r, g, b)
+            
+    def render_time(hours, minutes, seconds):
+    
+        # Top hour lamps
+        for i in range(4):
+            if i < hours//5:
+                draw_lamp(i*4,1,3,1,255,153,0)
+            else:
+                draw_lamp(i*4,1,3,1,20,20,20)
+        
+        # Bottom hour lamps
+        for i in range(4):
+            if i < hours%5:
+                draw_lamp(i*4,2,3,1,255,153,102)
+            else:
+                draw_lamp(i*4,2,3,1,20,20,20)
+        
+        
+        # Top minute lamps
+        for i in range(11):
+            if i < minutes//5:
+                if i>0 and (i+1)%3 == 0:
+                    draw_lamp(i,4,1,1,0,17,238)
+                else:
+                    draw_lamp(i,4,1,1,58,75,126)                
+            else:
+                draw_lamp(i,4,1,1,20,20,20)
+        
+        # Bottom minute lamps
+        for i in range(4):
+            if i < minutes%5:
+                draw_lamp(i*4,5,3,1,51,102,255)
+            else:
+                draw_lamp(i*4,5,3,1,20,20,20)
+                
+        # Seconds blinker
+        for i in range(14):
+            if seconds == 255: # Hack for settime
+                draw_lamp(1+i,3,1,1,0,0,255)
+            else:
+                if seconds % 2:
+                    draw_lamp(1+i,3,1,1,204,102,153)
+                else:
+                    draw_lamp(1+i,3,1,1,20,20,20)            
+
+    
+    while not unicorn.is_pressed(unicorn.BUTTON_Y):
+        current_tick = utime.ticks_ms()
+        handling_button_a = unicorn.is_pressed(unicorn.BUTTON_A) and not (current_tick - handling_a_ts) < BUTTON_IDLE_TIME 
+        handling_button_b = unicorn.is_pressed(unicorn.BUTTON_B) and not (current_tick - handling_b_ts) < BUTTON_IDLE_TIME
+        handling_button_x = unicorn.is_pressed(unicorn.BUTTON_X) and not (current_tick - handling_x_ts) < BUTTON_IDLE_TIME
+
+        if handling_button_x:
+            if mode == MODE_SETTIME:
+                clock.datetime((2023,7,15,0,hours_to_set, minutes_to_set,0,0))
+                mode = MODE_RUNCLOCK
+            else:
+                hours_to_set = clock.datetime()[4]
+                minutes_to_set = clock.datetime()[5]
+                mode = MODE_SETTIME
+            handling_x_ts = current_tick
+        
+              
+        if mode == MODE_SETTIME:
+                    
+            if handling_button_a:
+                hours_to_set = (hours_to_set+1)%24
+                handling_a_ts = current_tick 
+            
+            if handling_button_b:
+                minutes_to_set = (minutes_to_set+1)%60
+                handling_b_ts = current_tick
+            
+            render_time(hours_to_set, minutes_to_set, 255)
+        
+        else:
+            current_time = clock.datetime()
+            render_time(current_time[4], current_time[5], current_time[6])
+        
+
 while True:
     while unicorn.is_pressed(unicorn.BUTTON_X):
         pomodoro()
     while unicorn.is_pressed(unicorn.BUTTON_A):
         supercomputer()
     while unicorn.is_pressed(unicorn.BUTTON_B):
-        flames()
+        berlin_clock()
     while unicorn.is_pressed(unicorn.BUTTON_Y):
         clrscr()
